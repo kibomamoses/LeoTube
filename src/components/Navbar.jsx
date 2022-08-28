f0a72a98f1dc652327833048aca5f6a09d364a4e
kibomamoses <kibomamoses100@gmail.com>
2022-08-28 10:19:30 -0400

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#202A44', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} /> <h1>LeoTube</h1>
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': 'ed327fd6cfmshdcc2e4ec3239f2bp1a05aejsn9dcb3836dcd7',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+
+  },
+  
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
